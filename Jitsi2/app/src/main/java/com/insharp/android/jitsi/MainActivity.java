9f1d775ba8a4c85833c1aecb85d4c2472ec0170f
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-12-15 20:47:50 +0530

    develop jitsi sample app

---
+++ Jitsi2/app/src/main/java/com/insharp/android/jitsi/MainActivity.java
@@ -0,0 +1,45 @@
+package com.insharp.android.jitsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(new URL(""))
+                    .setRoom("test123")
+                    .setAudioMuted(false)
+                    .setVideoMuted(false)
+                    .setAudioOnly(false)
+                    .setWelcomePageEnabled(false)
+                    .build();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onButtonClick(View view) {
+        EditText editText = findViewById(R.id.conferenceName);
+        String text = editText.getText().toString();
+
+        if (text.length() > 0) {
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder().setRoom(text).build();
+            JitsiMeetActivity.launch(this,options);
+        }
+    }
+}
